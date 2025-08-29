12ef07b0e0d9ece5fdf6da01b736b49d64912a74
David <davidamedinahx@gmail.com>
2025-08-28 19:06:38 -0500

    feat: :sparkles: Modificaciones al código por mi compañero de grupo.
    Modificaciones al código por mi compañero de grupo.

--- models/userModel.js
+++ models/userModel.js
@@ -2,9 +2,14 @@ import { client } from '../config/db.js';
 import { ObjectId } from 'mongodb';
 
 export const createUser = async (userData) => {
-  const db = client.db('FreakHub');
+  const db = await connectDB();
   const usersCollection = db.collection('users');
-  return await usersCollection.insertOne(userData);
+  return await usersCollection.insertOne({
+    ...userData,
+    role: userData.role || 'usuario',
+    reviews: [], 
+    createdAt: new Date()
+  });
 };
 
 export const findUserByEmail = async (email) => {
